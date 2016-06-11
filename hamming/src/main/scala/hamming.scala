48a3540191d148d3574829c962649eb16d055391
Bill Cruickshank <jestingrabbit@gmail.com>
2016-06-11 11:23:52 +1000

    gone through some iterations with the other ones, submitted pangram

--- hamming/src/main/scala/hamming.scala
+++ hamming/src/main/scala/hamming.scala
@@ -1,8 +1,6 @@
 object Hamming {
   def compute(str1: String, str2: String): Int = {
-    if (str1.length != str2.length) {
-      throw new java.lang.IllegalArgumentException
-    }
-    str1.toList.zip(str2.toList).filter( {case (c1, c2) => c1 != c2 } ).length
-  } // a lot of action in the last line, but I think its pretty clear.
+    require(str1.length == str2.length)
+    (str1 zip str2).count( {case (c1, c2) => c1 != c2 } )
+  }
 }
--- word-count/src/main/scala/word_count.scala
+++ word-count/src/main/scala/word_count.scala
@@ -1,21 +1,14 @@
 class Phrase(phrase: String) {
 
-  val normalizedPhrase = phrase.toLowerCase.replaceAll(",", " ").replaceAll("[\\p{Punct}&&[^']]", "")
+  val normalizedPhrase = phrase
+                           .toLowerCase
+                           .replaceAll(",", " ")
+                           .replaceAll("[\\p{Punct}&&[^']]", "")
+                           .split("\\s+")
+                           .toList
 
-  def wordCount: Map[String, Int] = {
-    def recursiveWordCount (list: List[String],
-                         count: Map[String, Int])
-                         : Map[String, Int]
-      = list match {
-      case word :: rest => {
-        if (count.contains(word)) {
-          recursiveWordCount( rest, count + (word -> (count(word) + 1)) );
-        } else {
-          recursiveWordCount( rest, count + (word -> 1));
-        }
-      }
-      case Nil => count
-    }
-    recursiveWordCount( normalizedPhrase.split("\\s+").toList, Map() );
-  }
+  val wordCount = normalizedPhrase
+                    .map( {word => (word, normalizedPhrase.count {otherWord => word == otherWord})})
+                    .toMap
+
 }
